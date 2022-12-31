1df3fc31ac83be735cccfdad2b998234426b53a6
catayon <121713727+catayon@users.noreply.github.com>
2023-01-01 00:21:19 +0530

    Create 1692A_Marathon.c

---
+++ 1692A_Marathon.c
@@ -0,0 +1,30 @@
+//1692A
+//Marathon
+
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
+
+int main()
+{ 
+    int n;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        int a[4];
+        int count=0;
+        for (int j = 0; j < 4; j++)
+        {
+            scanf("%d", &a[j]);
+        }
+        for (int j = 1; j < 4; j++)
+        {
+            if (a[0]<a[j])
+            {
+                count++;
+            }
+        }
+        printf("%d\n", count);  
+    }
+    return 0;
+}
