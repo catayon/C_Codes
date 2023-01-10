4a7c0e18d76ba5121fa6ceb432f10f2755271de2
catayon <121713727+catayon@users.noreply.github.com>
2023-01-11 00:23:40 +0530

    Create 1206A_ChooseTwoNumbers.cpp

---
+++ 1206A_ChooseTwoNumbers.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{ 
+    int na,nb,p=0,q=0,c,cnta=0,cntb=0;
+    cin>>na;
+    int a[na];
+    for (int i = 0; i < na; i++)
+    {
+        cin>>a[i];
+    }
+    cin>>nb;
+    int b[nb];
+    for (int i = 0; i < nb; i++)
+    {
+        cin>>b[i];
+    }
+    for (int i = 0; i < na; i++)
+    {
+        for (int j = 0; j < nb; j++)
+        {
+            c=a[i]+b[j];
+            
+            for (int k = 0; k < na; k++)
+            {
+                if (c!=a[k])
+                {
+                    cnta++;
+                } 
+            }
+            for (int k = 0; k < nb; k++)
+            {
+                if (c!=b[k])
+                {
+                    cntb++;
+                } 
+            }
+            if (cnta==na && cntb==nb)
+            {
+                p=a[i];
+                q=b[j];
+                break;
+            }   
+        }    
+    }
+    cout<<p<<" "<<q<<endl;
+    return 0;
+}
