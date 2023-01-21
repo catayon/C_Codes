00e6f88cefaba3dec65cb7f3f167f7a5c97e9ab2
Tathya Patel <121713727+tathya1001@users.noreply.github.com>
2023-01-21 22:25:31 +0530

    Create 1740A_FactoriseN+M.cpp

---
+++ 1740A_FactoriseN+M.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t, n, m, p[100000], k = 0;
+    cin >> t;
+
+    for (int k = 0; k < t; k++)
+    {
+        cin >> n;
+
+        if (n == 2)
+        {
+            cout << 7 << endl;
+        }
+        else
+        {
+            cout << 5 << endl;
+        }
+    }
+    return 0;
+}
