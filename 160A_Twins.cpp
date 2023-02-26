9ffef1bbe4ba86fc7031b30634bf179855a22f3f
Tathya Patel <121713727+tathya1001@users.noreply.github.com>
2023-02-26 13:50:42 +0530

    Create 160A_Twins.cpp

---
+++ 160A_Twins.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, total = 0;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        total = total + a[i];
+    }
+
+    sort(a, a + n);
+
+    int you_have = 0, twin_has = total, coinstaken = 0;
+    
+    for (int i = n - 1; i >= 0; i--)
+    {
+        you_have = you_have + a[i];
+        twin_has = twin_has - a[i];
+        coinstaken++;
+        if (you_have > twin_has)
+        {
+            break;
+        }
+    }
+
+    cout << coinstaken;
+
+    return 0;
+}
