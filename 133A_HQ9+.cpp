0fb0212aa614664948832c69dd1bdb1ff2038359
Tathya Patel <121713727+tathya1001@users.noreply.github.com>
2023-03-07 23:24:26 +0530

    Create 133A_HQ9+.cpp

---
+++ 133A_HQ9+.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cin >> s;
+
+    bool passed = false;
+
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[i] == 'H' || s[i] == 'Q' || s[i] == '9')
+        {
+            passed = true;
+            break;
+        }
+    }
+    if (passed)
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+
+    return 0;
+}
