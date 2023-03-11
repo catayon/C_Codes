393a35170a57d0e4d62373b3cf23d0356973e23e
Tathya Patel <121713727+tathya1001@users.noreply.github.com>
2023-03-11 22:40:01 +0530

    Create 339A_HelpfulMaths.cpp

---
+++ 339A_HelpfulMaths.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cin >> s;
+    vector<int> a;
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[i] != '+')
+        {
+            a.push_back(s[i] - 48);
+        }
+    }
+    sort(a.begin(), a.end());
+    for (int i = 0; i < a.size(); i++)
+    {
+        cout << a[i];
+        if (i == a.size() - 1)
+        {
+            break;
+        }
+        cout << '+';
+    }
+    return 0;
+}
