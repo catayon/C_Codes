80bfaa9670afde1f4651367321ecb8fd04d57a7b
Tathya Patel <121713727+tathya1001@users.noreply.github.com>
2023-10-07 18:57:44 +0530

    Create 1772A_A+B?.cpp

---
+++ 1772A_A+B?.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve()
+{
+    int a, b;
+    char c;
+    cin >> a >> c >> b;
+    cout << a + b << endl;
+}
+int main()
+{
+    int t = 1;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
