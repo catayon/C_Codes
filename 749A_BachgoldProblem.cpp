7bdd4b0ba97553e810c7128de3f68f73aca711eb
Tathya Patel <121713727+tathya1001@users.noreply.github.com>
2023-10-25 19:39:16 +0530

    Create 749A_BachgoldProblem.cpp

---
+++ 749A_BachgoldProblem.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    cout << n / 2 << endl;
+    for (int i = 0; i < n / 2 - 1; i++)
+        cout << 2 << " ";
+    cout << (n % 2 ? 3 : 2);
+    return 0;
+}
